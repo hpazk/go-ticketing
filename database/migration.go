0e3a4ed026cc9c341011d9dce7e9307780e32e09
hpazk <hpazk.zkh@gmail.com>
2021-04-21 08:43:24 +0700

    start new project

--- database/migration.go
+++ database/migration.go
@@ -11,27 +11,6 @@ import (
 
 func GetMigrations(db *gorm.DB) *gormigrate.Gormigrate {
 	return gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
-		// {
-		// 	ID: "2020080201",
-		// 	Migrate: func(tx *gorm.DB) error {
-		// 		if err := tx.AutoMigrate(&user.User{}).Error; err != nil {
-		// 			return err
-		// 		}
-		// 		if err := tx.AutoMigrate(&book.Book{}).Error; err != nil {
-		// 			return err
-		// 		}
-		// 		return nil
-		// 	},
-		// Rollback: func(tx *gorm.DB) error {
-		// 	if err := tx.DropTable("blogs").Error; err != nil {
-		// 		return nil
-		// 	}
-		// 	if err := tx.DropTable("users").Error; err != nil {
-		// 		return nil
-		// 	}
-		// 	return nil
-		// },
-		// },
 		{
 			ID: fmt.Sprintf("%d", time.Now().Unix()),
 			Migrate: func(tx *gorm.DB) error {
@@ -44,4 +23,24 @@ func GetMigrations(db *gorm.DB) *gormigrate.Gormigrate {
 	})
 }
 
-// 1618796640
+// {
+// 	ID: "2020080201",
+// 	Migrate: func(tx *gorm.DB) error {
+// 		if err := tx.AutoMigrate(&user.User{}).Error; err != nil {
+// 			return err
+// 		}
+// 		if err := tx.AutoMigrate(&book.Book{}).Error; err != nil {
+// 			return err
+// 		}
+// 		return nil
+// 	},
+// Rollback: func(tx *gorm.DB) error {
+// 	if err := tx.DropTable("blogs").Error; err != nil {
+// 		return nil
+// 	}
+// 	if err := tx.DropTable("users").Error; err != nil {
+// 		return nil
+// 	}
+// 	return nil
+// },
+// },
